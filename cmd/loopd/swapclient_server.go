5f67c6b64464d8ae41cd5e129df15b46bf689d51
Johan T. Halseth <johanth@gmail.com>
2019-09-23 13:07:32 +0200

    looprpc: add SwapPublicationDeadline to client proto
    cmd/loopd swapclient_server: use SwapPublicationDeadline from LoopOutRequest

--- cmd/loopd/swapclient_server.go
+++ cmd/loopd/swapclient_server.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"sort"
+	"time"
 
 	"github.com/lightningnetwork/lnd/queue"
 
@@ -68,14 +69,15 @@ func (s *swapClientServer) LoopOut(ctx context.Context,
 	}
 
 	req := &loop.OutRequest{
-		Amount:              btcutil.Amount(in.Amt),
-		DestAddr:            sweepAddr,
-		MaxMinerFee:         btcutil.Amount(in.MaxMinerFee),
-		MaxPrepayAmount:     btcutil.Amount(in.MaxPrepayAmt),
-		MaxPrepayRoutingFee: btcutil.Amount(in.MaxPrepayRoutingFee),
-		MaxSwapRoutingFee:   btcutil.Amount(in.MaxSwapRoutingFee),
-		MaxSwapFee:          btcutil.Amount(in.MaxSwapFee),
-		SweepConfTarget:     sweepConfTarget,
+		Amount:                  btcutil.Amount(in.Amt),
+		DestAddr:                sweepAddr,
+		MaxMinerFee:             btcutil.Amount(in.MaxMinerFee),
+		MaxPrepayAmount:         btcutil.Amount(in.MaxPrepayAmt),
+		MaxPrepayRoutingFee:     btcutil.Amount(in.MaxPrepayRoutingFee),
+		MaxSwapRoutingFee:       btcutil.Amount(in.MaxSwapRoutingFee),
+		MaxSwapFee:              btcutil.Amount(in.MaxSwapFee),
+		SweepConfTarget:         sweepConfTarget,
+		SwapPublicationDeadline: time.Unix(int64(in.SwapPublicationDeadline), 0),
 	}
 	if in.LoopOutChannel != 0 {
 		req.LoopOutChannel = &in.LoopOutChannel
